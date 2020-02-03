d9dee606f2befa3661c0cc3f75f98c60a2b9b8df
Chi Mei Wang <kimmy0929@gmail.com>
2020-02-04 00:59:02 +0900

    add form with post request

--- app/controllers/taskList1.scala
+++ app/controllers/taskList1.scala
@@ -5,6 +5,23 @@ import play.api.mvc._
 
 @Singleton
 class taskList1 @Inject()(val controllerComponents: ControllerComponents) extends BaseController{
+  def login = Action {
+    Ok(views.html.login1())
+  }
+
+  def valdiateLoginGet(username: String, password: String) = Action {
+    Ok(s"$username logged in with $password")
+  }
+
+  def valdiateLoginPost = Action {  request =>
+    val postVals = request.body.asFormUrlEncoded
+    postVals.map { args =>
+      val username = args("username").head
+      val password = args("password").head
+      Ok(s"$username logged in with $password")
+    }.getOrElse(Ok("Oh nooo"))
+  }
+
   def taskList = Action {
     val tasks = List("task 1", "task 2", "task 3")
     Ok(views.html.taskList1(tasks))
